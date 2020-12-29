98b3ab5758f27dbde64ae68115e4a22504d43119
Pragya Singh <pragyasingh991@gmail.com>
2020-12-29 18:21:44 +1100

    run graphql schema using apollo server

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+  type Cat {
+    name: String
+  }
+  type Query {
+    myCat: Cat
+  }
     schema {
       query: Query
     }
@@ -19,7 +25,11 @@ export const start = async () => {
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: {
+      Query: {
+        myCat: () => ({name: 'Kitty'})
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
